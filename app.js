f0ec053453397c413c8d92076cc4e9d460909d37
SudheerReddySingam <54614069+SudheerReddySingam@users.noreply.github.com>
2019-09-20 11:21:43 +0530

    updated obstacles

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
-var a = canvas.width-30;
-var b = canvas.height-4;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
-function drawBall() {
+var a=80,b=40,c=30,d=18,e=80,f=210,g=140,h=250;
+var da = 8;
+var db = 2;
+var dc = 4;
+var dd = 8;
+var de = 4;
+var df = 2;
+var dg = 4;
+var dh = 2;
+function drawBars() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.arc(a, b, ballRadius, 0, Math.PI*2);
+    ctx.rect(a,40,80,20);
+    ctx.rect(b,90,60,20);
+    ctx.rect(c,140,40,20);
+    ctx.rect(d,190,60,20);
+    ctx.rect(e,240,60,20);
+    ctx.rect(f,290,60,20);
+    ctx.rect(g,340,60,20);
+    ctx.rect(h,390,60,20);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -18,17 +26,38 @@ function drawBall() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    x += dx;
-    y += dy;
-    a += dx;
-    b += dy;
-    if(x + dx > canvas.width || x + dx < 0 || a + dx > canvas.width || a + dx < 0) {
-        dx = -dx;
+    drawBars();
+    a += da;
+    b += db;
+    c += dc;
+    d += dd;
+    e += de;
+    f += df;
+    g += dg;
+    h += dh;
+    if(a + da+40 > canvas.width || a + da < 0) {
+        da = -da;
     }
-    
-    if(y + dy > canvas.height || y + dy < 0 || b + dy > canvas.height || b + dy < 0) {
-        dy = -dy;
+    if(b + db+60 > canvas.width || b + db < 0) {
+        db = -db;
+    }
+    if(c + dc+40 > canvas.width || c + dc < 0) {
+        dc = -dc;
+    }
+    if(d + dd+60 > canvas.width || d + dd < 0) {
+        dd = -dd;
+    }
+    if(e + de+40 > canvas.width || e + de < 0) {
+        de = -de;
+    }
+    if(f + df+60 > canvas.width || f + df < 0) {
+        df = -df;
+    }
+    if(g + dg+40 > canvas.width || g + dg < 0) {
+        dg = -dg;
+    }
+    if(h + dh+60 > canvas.width || h + dh < 0) {
+        dh = -dh;
     }
 }
 setInterval(draw, 10);
